30c431c0ab67e57168d3e12f97d4085357f8893c
Vikhyat09 <f20220327@goa.bits-pilani.ac.in>
2025-04-26 23:04:56 +0530

    Update AnalyticsProvider.java

--- OOP Project 2/AnalyticsProvider.java
+++ OOP Project 2/AnalyticsProvider.java
@@ -4,5 +4,5 @@ package com.influencer.platform;
  * Provides analytics functionality
  */
 public interface AnalyticsProvider {
-    void generateAnalyticsReport(Campaign campaign);  // Campaign performance stats
+     // Campaign performance stats
 }
