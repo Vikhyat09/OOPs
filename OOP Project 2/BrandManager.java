81f041a363638eff80ed23a4760bd670b0589ea3
Praneethj39 <praneethj39@gmail.com>
2025-04-26 20:39:34 +0530

    Update BrandManager.java

--- OOP Project 2/BrandManager.java
+++ OOP Project 2/BrandManager.java
@@ -1,6 +1,39 @@
 package com.influencer.platform;
 
 public class BrandManager extends User {
+  private final String name;
+        private double availableBudget;
+        private Contract[] contracts = new Contract[10];
+        private int contractCount = 0;
+
+        public BrandManager(String name, double budget) {
+            this.name = name;
+            this.availableBudget = budget;
+        }
+
+        public void addContract(Contract contract) {
+            if (contractCount == contracts.length) {
+                Contract[] newArray = new Contract[contracts.length * 2];
+                // Replace arraycopy with manual copy
+                for (int i = 0; i < contracts.length; i++) {
+                    newArray[i] = contracts[i];
+                }
+                contracts = newArray;
+            }
+            contracts[contractCount++] = contract;
+        }
+
+        public Contract[] getContracts() {
+            Contract[] result = new Contract[contractCount];
+            // Replace arraycopy with manual copy
+            for (int i = 0; i < contractCount; i++) {
+                result[i] = contracts[i];
+            }
+            return result;
+        }
+
+
+  
 public Campaign createCampaign(String name, Double budget) { /* Setup new campaign */ }
 public void filterInfluencers(String niche, Double minEngagement) { /* Search influencers */ }
 }
