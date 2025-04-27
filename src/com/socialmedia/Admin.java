088837b3fab13fc04fc2a39f45b87abc20550eb3
arhansd1 <sdarhan2002a@gmail.com>
2025-04-27 05:51:02 +0530

    Update Admin.java

--- src/com/socialmedia/Admin.java
+++ src/com/socialmedia/Admin.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Admin extends User {
     private ArrayList<User> userList;
-
+//static set of all niches in which the applied niche should excist 
     private static final String[] NICHES = {
         "FITNESS", "FOOD", "TRAVEL", "BEAUTY", "FASHION", "TECH", "GAMING"
     };
@@ -14,13 +14,13 @@ public class Admin extends User {
         super(username, password, email, "Admin", null);
         this.userList = new ArrayList<>();
     }
-
+//adds a user 
     public void addUser(String username, String password, String email, String role) {
         User newUser = null;
 
         // Randomly assign a niche
         String niche = getRandomNiche();
-
+//saves the user in the specific class based on the role
         switch (role) {
             case "Admin":
                 newUser = new Admin(username, password, email);
@@ -46,7 +46,7 @@ public class Admin extends User {
                 + ", Followers: " + influencer.getFollowers());
         }
     }
-
+//removes a certain user 
     public void removeUser(String username) {
         User userToRemove = null;
         for (User user : userList) {
@@ -62,20 +62,21 @@ public class Admin extends User {
             System.out.println("User not found: " + username);
         }
     }
-
+//list of all users
     public void viewAllUsers() {
         System.out.println("All registered users:");
         for (User user : userList) {
             System.out.println("- Username: " + user.getUsername() + ", Role: " + user.getRole() + ", Niche: " + user.getNiche());
         }
     }
-
+//applies a randomised niche to each user (if its a influencer or brandmanager)
     private String getRandomNiche() {
         Random random = new Random();
         int index = random.nextInt(NICHES.length);
         return NICHES[index];
     }
 
+    //gives us the overview of the key features in Admin
     @Override
     public void viewDashboard() {
         System.out.println("Admin Dashboard - Manage Users and View Platform Analytics");
