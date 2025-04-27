31a4e2fce88936096e3db5bfd466cad7ab0b665d
arhansd1 <sdarhan2002a@gmail.com>
2025-04-27 06:48:33 +0530

    Update UserDatabase.java

--- src/com/socialmedia/UserDatabase.java
+++ src/com/socialmedia/UserDatabase.java
@@ -1,7 +1,7 @@
 package com.socialmedia;
 
 import java.util.ArrayList;
-
+// stores data of all the users in the database
 public class UserDatabase {
     private static ArrayList<User> users = new ArrayList<>();
 
